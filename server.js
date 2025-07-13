cc1c7c4282056ae9378e26e11c94d3876654b7d9
wazid33 <mdwazidhussain68@gmail.com>
2025-07-13 20:16:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,8 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI);
-
-.then(() => console.log('✅ Connected to MongoDB'))
-.catch(err => console.error('❌ MongoDB connection error:', err));
+   .then(() => console.log('✅ Connected to MongoDB'))
+   .catch(err => console.error('❌ MongoDB connection error:', err));
 
 // Sample route
 app.get('/', (req, res) => {
